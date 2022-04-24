62140fe62cb49eed2900ee1fbcdc6359de887cb4
Mansur2001 <62624233+Mansur2001@users.noreply.github.com>
2022-04-24 03:47:10 -0700

    Add files via upload

--- FullStack Learn/javascript-course-repo/index.js
+++ FullStack Learn/javascript-course-repo/index.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+function readForm(){
+    //the content of your function will go here
+    var radioButtons = document.getElementsByName("[name property value for the buttons]")
+
+    for (var i = 0; i < radioButtons.length; i++){
+        //tasks go here
+        if (radioButtons[i].checked == true){
+            //actions go here
+            radioButtons[i].value
+            document.getElementById().innerHTML
+          }
+      }
+  }
